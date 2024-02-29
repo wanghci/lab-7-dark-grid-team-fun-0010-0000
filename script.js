deccd7a871c90e32976d4607150f1d8c3072a9d3
Eleni H <eleni.mariah04@gmail.com>
2024-02-29 16:32:09 -0500

    finished

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 
-document.getElementById("darkbutton").addEventListener("click", go_dark());
-document.getElementById("lightbutton").addEventListener("click", go_light());
-document.getElementById("defbutton").addEventListener("click", go_OS());
+document.getElementById("darkbutton").addEventListener("click", go_dark);
+document.getElementById("lightbutton").addEventListener("click", go_light);
+document.getElementById("defbutton").addEventListener("click", go_OS);
 
 function go_dark() {
     document.body.classList.remove("debug");
-    document.body.classList.remove("OS");
     document.body.classList.add("dark");
 }
 
 function go_light() {
     document.body.classList.remove("dark");
-    document.body.classList.remove("OS");
     document.body.classList.add("debug");
 }
 
 function go_OS() {
     document.body.classList.remove("dark");
     document.body.classList.remove("debug");
-    document.body.classList.add("OS");
+    //create media query for light copy paste all debug things
 }
