c00a82b872a65de1c0def388ac26e0d39b1c1652
Eleni H <eleni.mariah04@gmail.com>
2024-02-29 15:42:13 -0500

    messed up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
-document.getElementById("darkbutton").addEventListener("click", ()=> document.body.classList.replace("debug","dark"));
-document.getElementById("lightbutton").addEventListener("click", ()=>document.body.classList.replace("dark","debug"));
-document.getElementById("defaultbutton").addEventListener("click", ); //computer default
+document.getElementById("darkbutton").addEventListener("click", ()=> document.body.classList.remove("dark"));
+document.getElementById("lightbutton").addEventListener("click", ()=>document.body.classList.remove("light"));
+document.getElementById("defaultbutton").addEventListener("click", ()=> document.body.classList.remove("dark","light") ); //computer default
