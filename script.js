88f4cb4781631bece434e8adbd05b916bfcb2928
Eleni H <eleni.mariah04@gmail.com>
2024-02-29 15:06:59 -0500

    my changes

---
+++ script.js
@@ -0,0 +1,4 @@
+
+document.getElementById("darkbutton").addEventListener("click", document.body.classList.replace("light","dark"));
+document.getElementById("lightbutton").addEventListener("click", document.body.classList.replace("dark","light"));
+document.getElementById("defaultbutton").addEventListener("click", displayDate);
