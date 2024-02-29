7d6969286d858aebeac5c0f9f17906375c965060
Eleni H <eleni.mariah04@gmail.com>
2024-02-29 15:18:30 -0500

    colors are showing up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
-document.getElementById("darkbutton").addEventListener("click", document.body.classList.replace("light","dark"));
-document.getElementById("lightbutton").addEventListener("click", document.body.classList.replace("dark","light"));
-document.getElementById("defaultbutton").addEventListener("click", displayDate);
+document.getElementById("darkbutton").addEventListener("click", ()=> document.body.classList.replace("debug","dark"));
+document.getElementById("lightbutton").addEventListener("click", ()=>document.body.classList.replace("dark","debug"));
+document.getElementById("defaultbutton").addEventListener("click", ); //computer default
